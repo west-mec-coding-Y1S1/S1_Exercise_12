9f66b2173d694a4b3eb34710348fc4867fc903b1
Patrick Clawson <patrick.clawson@west-mec.org>
2019-02-25 15:58:56 -0700

    More updates

--- lht_calendar.js
+++ lht_calendar.js
@@ -40,6 +40,7 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 function createCalendar(calDate) {
    var calendarHTML = "<table id='calendar_table'>";
    calendarHTML += calCaption(calDate);
+   calendarHTML += calWeekdayRow();
    calendarHTML += "</table>";
    return calendarHTML;
 }
@@ -57,4 +58,37 @@ function calCaption(calDate) {
 
    //write the caption
    return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
+}
+
+// function to write a table row of weekday abbreviations
+function calWeekdayRow() {
+   //array of weekday abbreviations
+   var dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+   var rowHTML = "<tr>";
+
+   //look through the dayName array
+   for (var i = 0; i < dayName.length; i++) {
+      rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
+   }
+   rowHTML += "</tr>";
+   return rowHTML;
+}
+
+// function to calculate the number of days in the month
+
+function daysInMonth(calDate) {
+   //array of days in each month
+   var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+   //extract the four digit year and month value
+   var thisYear = calDate.getFullYear();
+   var thisMonth = calDate.getMonth();
+
+   //revise the days in February for leap years
+   if (thisYear % 4 === 0) {
+      dayCount[1] = 29;
+   }
+
+   //return the number of days for the current month
+   return dayCount[thisMonth];
 }
